d23bc085863a0b8caa77a4b93df20f4f96368315
jonfarias <jonathan.developer10@gmail.com>
2022-02-07 01:28:55 -0300

    updates

--- chat/application/test/test_basic.py
+++ chat/application/test/test_basic.py
@@ -2,7 +2,7 @@ from flask import abort, url_for
 import os
 
 def test_app_is_created(app):
-        assert app.name == "chat.app"
+        assert app.name == "application.app"
 
 def test_request_returns_404(client):
         assert client.get("/url_que_nao_existe").status_code == 404
