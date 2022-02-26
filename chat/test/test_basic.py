b0faf5bcb019dcd4ac60cfcfe47814a2cedec6c0
jonfarias <jonathan.developer10@gmail.com>
2022-02-26 02:26:23 -0300

    testes

--- chat/test/test_basic.py
+++ chat/test/test_basic.py
@@ -1,7 +1,8 @@
 from flask import abort, url_for
 import os
-from application.models import User
-from application.app import db
+from sqlalchemy import create_engine
+from flask_login import UserMixin
+
 
 def test_app_is_created(app):
         assert app.name == "application.app"
@@ -10,21 +11,12 @@ def test_request_returns_404(client):
         assert client.get("/url_que_nao_existe").status_code == 404
 
 def test_homepage_view(client):
-        """
-        Test that homepage is accessible without login
-        """
         assert client.get(url_for('main.index')).status_code == 200 
        
 def test_login_view(client):
-        """
-        Test that login page is accessible without login
-        """
         assert client.get(url_for('auth.login')).status_code == 200
 
 def test_signup_view(client):
-        """
-        Test that login page is accessible without login
-        """
         assert client.get(url_for('auth.signup')).status_code == 200 
 
 def login(client, username, password):
@@ -37,9 +29,17 @@ def login(client, username, password):
 def logout(client):
     assert client.get(url_for('auth.logout'), follow_redirects=True)
 
+
+class User(UserMixin):
+    id = db.Column(db.Integer, primary_key=True) 
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
 def user():
-        db.create_all()
+        engine = create_engine('sqlite://')
+        engine.create_all()
         new_user = User(email=teste@gmail.com, name=Teste, password=generate_password_hash(teste12345, method='sha256'))
-        db.session.add(new_user)
-        db.session.commit()
+        engine.session.add(new_user)
+        engine.session.commit()
         assert User.query.filter_by(email=teste@gmail.com).first()
